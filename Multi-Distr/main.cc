834bd0d0851d806eee65a35ffa39827f463de033
Raymond-CL <raymond.chen88@outlook.com>
2024-12-20 11:28:24 +0100

    got rid of class

--- Multi-Distr/main.cc
+++ Multi-Distr/main.cc
@@ -2,119 +2,125 @@
 #include <math.h>
 
 #include <iostream>
+#include <fstream>
 
+const double Nc = 3;
+const double Nf = 3;
+const double b = 11.0 / 3.0 * Nc - 2.0 / 3.0 * Nf;
+const double Lqcd = 0.25;
+const double Q0 = 0.5;
+const double lambda = log(Q0 / Lqcd);
+const double ymin = 0.0;
 const double ymax = 10.0;
-const int nmax = 5;
-const int ny = 51;
-double Pn_table[nmax][ny];
-double Sn_table[nmax][ny];
+const int Nn = 2;
+const int Ny = 21;
+double Pny_table[Nn+1][Ny];
+double Sny_table[Nn+1][Ny];
 
-inline double gamma(double yp) {
-  const double Nc = 3;
-  const double Nf = 3;
-  const double b = 11.0 / 3.0 * Nc - 2.0 / 3.0 * Nf;
-  const double Lqcd = 0.25;
-  const double Q0 = 0.5;
-  const double lambda = log(Q0 / Lqcd);
-  return 4.0 * Nc / b / (yp + lambda);
-}
+struct input_params {
+  int n_now;
+  double y_now;
+};
 
-double f(double yp, void* params) {
-  double y = *(double*)params;
-  return (y - yp) * gamma(yp);
-}
+void initialize();
+void print_Pny(std::ostream &o_obj);
+inline double gamma(double yp) { return 4.0 * Nc / b / (yp + lambda); }
 
-double P1(double y) {
-  size_t n = 10;
-  gsl_integration_workspace* w = gsl_integration_workspace_alloc(n);
-  double ypmin = 0.0;
-  double ypmax = y;
-  double epsabs = 0.0;
-  double epsrel = 0.1;
-  double result, error;
-  gsl_function F;
-  F.function = &f;
-  F.params = &y;
-  gsl_integration_qag(&F, ypmin, ypmax, epsabs, epsrel, n, GSL_INTEG_GAUSS15, w,
-                      &result, &error);
-  gsl_integration_workspace_free(w);
-  return exp(-result);
-}
-
-double linear_interpolate(const double* x, const double* y, int size,
-                          double x_value) {
-  if (size < 2) {
-    throw std::invalid_argument(
-        "Array size must be at least 2 for interpolation.");
-  }
-
-  // Handle the case where x_value is out of bounds
-  if (x_value <= x[0]) {
-    return y[0];
-  }
-  if (x_value >= x[size - 1]) {
-    return y[size - 1];
-  }
-
-  // Find the interval [x[i], x[i+1]] that contains x_value
+double get_Pny(int n, double y) {
   int i = 0;
-  while (x_value > x[i + 1] && i < size - 2) {
+  while (i < Ny - 1 && y > Pny_table[0][i]) {
     i++;
   }
-
-  // Perform linear interpolation
-  double x0 = x[i];
-  double x1 = x[i + 1];
-  double y0 = y[i];
-  double y1 = y[i + 1];
-
-  return y0 + (y1 - y0) * (x_value - x0) / (x1 - x0);
+  if (i == 0) {
+    return Pny_table[n][0];
+  } else if (i == Ny - 1) {
+    return Pny_table[n][Ny - 1];
+  } else {
+    double x1 = Pny_table[0][i - 1];
+    double x2 = Pny_table[0][i];
+    double y1 = Pny_table[n][i - 1];
+    double y2 = Pny_table[n][i];
+    return y1 + (y - x1) * (y2 - y1) / (x2 - x1);
+  }
 }
 
-double g(double yp, void* params) {
-  double y = *(double*)params;
-  return (y - yp) * gamma(yp) *
-         linear_interpolate(Pn_table[0], Pn_table[1], ny, yp);
+double Sny_int(double yp, void *params) {
+  struct input_params *p = (struct input_params *)params;
+  int n = p->n_now;
+  double y = p->y_now;
+  if (n == 1) {
+    return (y - yp) * gamma(yp);
+  } else {
+    return (y - yp) * gamma(yp) * get_Pny(n - 1, y);
+  }
 }
 
-double S1(double y) {
-  size_t n = 10;
-  gsl_integration_workspace* w = gsl_integration_workspace_alloc(n);
-  double ypmin = 0.0;
-  double ypmax = y;
-  double epsabs = 0.0;
-  double epsrel = 0.1;
+void set_Sny(int n) {
+  // prepare integrtion workspace
+  int npt = 1000;
   double result, error;
+  double abserr = 1.0e-8;
+  double relerr = 1.0e-8;
+  gsl_integration_workspace *w = gsl_integration_workspace_alloc(npt);
   gsl_function F;
-  F.function = &g;
-  F.params = &y;
-  gsl_integration_qag(&F, ypmin, ypmax, epsabs, epsrel, n, GSL_INTEG_GAUSS15, w,
-                      &result, &error);
+  F.function = &Sny_int;
+
+  for (int i = 0; i < Ny; i++) {
+    double ypmin = ymin;
+    double ypmax = Pny_table[n - 1][i];
+    struct input_params ip = {n, ypmax};
+    F.params = &ip;
+    gsl_integration_qag(&F, ypmin, ypmax, abserr, relerr, npt,
+                        GSL_INTEG_GAUSS15, w, &result, &error);
+    Sny_table[n - 1][i] = result;
+  }
+
   gsl_integration_workspace_free(w);
-  return result;
 }
+
+void set_Pny(int n) {
+  if (n == 1) {
+    for (int i = 0; i < Ny; i++) {
+      Pny_table[n][i] = exp(-Sny_table[n - 1][i]);
+    }
+  } else {
+    for (int i = 0; i < Ny; i++) {
+      for (int j = 1; j < n; j++) {
+        Pny_table[n][i] += j/(n-1)*Pny_table[n-j][i]*Sny_table[j][i];
+      }
+    }
+  }
+}
+
 
 int main(void) {
-  for (int i = 0; i < ny; i++) {
-    Pn_table[0][i] = i * ymax / (ny - 1);
+  // initialize y values
+  initialize();
+
+  // run table
+  for(int i=1;i<=Nn;i++){
+    set_Sny(i);
+    set_Pny(i);
   }
 
-  for (int i = 0; i < ny; i++) {
-    Pn_table[1][i] = P1(Pn_table[0][i]);
-  }
-
-  for (int i = 0; i < ny; i++) {
-    Pn_table[2][i] = S1(Pn_table[0][i]);
-  }
-
-  for (int i = 0; i < ny; i++) {
-    Pn_table[3][i] = Pn_table[1][i] * Pn_table[2][i];
-  }
-
-  for (int i = 0; i < ny; i++) {
-    std::cout << Pn_table[0][i] << '\t' << Pn_table[1][i] << '\t'
-              << Pn_table[3][i] << std::endl;
-  }
+  // print table
+  std::ofstream out_file("Pny_table.dat", std::ios::out);
+  // print_Pny(std::cout);
+  print_Pny(out_file);
+  out_file.close();
 
   return 0;
+}
+
+void initialize() {
+  for (int i = 0; i < Ny; i++) {
+    Pny_table[0][i] = i * ymax / (Ny - 1);
+  }
+}
+
+void print_Pny(std::ostream &o_obj) {
+  for (int i = 0; i < Ny; i++) {
+    for (int j = 0; j <= Nn; j++) o_obj << Pny_table[j][i] << " \t ";
+    o_obj << std::endl;
+  }
 }
